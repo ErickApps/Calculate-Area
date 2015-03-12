52926286fb926622fd9c44018257d1591128c8c3
Erick Barbosa <Erick@Ericks-iMac.local>
2015-03-11 22:18:09 -0400

    Initial Commit

---
+++ CalculatePerimeterUsingProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatePerimeterUsingProtocol
+//
+//  Created by Erick Barbosa on 3/11/15.
+//  Copyright (c) 2015 Erick_Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatePerimeterUsingProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalculatePerimeterUsingProtocol
+//
+//  Created by Erick Barbosa on 3/11/15.
+//  Copyright (c) 2015 Erick_Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
